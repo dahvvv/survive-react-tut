59347043b521f499d3b3a1d1d596fb1c4b7e82ac
Dave Koenig <davidjosephkoenig@gmail.com>
2016-03-30 10:53:23 -0400

    Move notes array into component state

--- app/components/App.js
+++ app/components/App.js
@@ -2,22 +2,26 @@ var React = require('react');
 var uuid = require('node-uuid');
 
 var App = React.createClass({
-	render: function(){
-		var notes = [
-			{
-				id:   uuid.v4(),
-				task: 'Learn Webpack'
-			},
-			{
-				id:   uuid.v4(),
-				task: 'Learn React'
-			},
-			{
-				id:   uuid.v4(),
-				task: 'Do Laundry'
-			}
-		];
-		var listItems = notes.map(function (note) {
+	getInitialState: function () {
+		return {
+			notes: [
+				{
+					id:   uuid.v4(),
+					task: 'Learn Webpack'
+				},
+				{
+					id:   uuid.v4(),
+					task: 'Learn React'
+				},
+				{
+					id:   uuid.v4(),
+					task: 'Do Laundry'
+				}
+			]
+		};
+	},
+	render: function () {
+		var listItems = this.state.notes.map(function (note) {
 			return (
 				<li key={note.id}>
 					{note.task}
