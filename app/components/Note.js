36833b82d795365fe29401a5fee75d7775abc86d
Dave Koenig <davidjosephkoenig@gmail.com>
2016-03-31 02:59:15 -0400

    Add input form to Note edit UI

--- app/components/Note.js
+++ app/components/Note.js
@@ -2,17 +2,25 @@ var React = require('react');
 
 var Note = React.createClass({
 	getInitialState: function () {
-		return { editing: false };
+		return {
+			editing: false,
+			inputValue: ''
+		};
 	},
 	edit: function () {
-		alert('yo');
 		this.setState({ editing: true });
+	},
+	handleInput: function (e) {
+		this.setState({ inputValue: e.target.value });
 	},
 	render: function () {
 		if (this.state.editing) {
 			return (
 				<div>
 					<h1>Edit the Note!</h1>
+					<input type="text" 
+									value={this.state.inputValue} 
+									onChange={this.handleInput} />
 				</div>
 			);
 		} else {
